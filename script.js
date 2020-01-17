bbf68629112f648f972a69e2ee671ba447a8087a
Monkata96 <sieroteev@gmail.com>
2020-01-17 15:32:29 +0200

    Added custom progrss bar and back to top button

--- script.js
+++ script.js
@@ -30,5 +30,39 @@ window.addEventListener('DOMContentLoaded', () => {
         typing();
     }, 200);
 
+    const backToTopBtn = document.querySelector('.backToTopBtn');
+
+    const checkYOffset = () => {
+        if(window.pageYOffset > 100) {
+            backToTopBtn.style.opacity = 1;
+            backToTopBtn.classList.remove('js-hide-btn')
+            backToTopBtn.classList.add('js-appear-btn');
+        } else {
+            backToTopBtn.style.opacity = 0;
+            backToTopBtn.classList.remove('js-appear-btn');
+            backToTopBtn.classList.add('js-hide-btn')
+        }
+    };
+
+    const backToTop = () => {
+        window.scrollTo(0, 0);
+    };
+    
+    window.addEventListener('scroll', checkYOffset);
+    backToTopBtn.addEventListener('click', backToTop)
+
+    const portfolioWrapper = document.querySelector('#portfolio-wrapper');
+    const progressBar = document.querySelector('.custom-progress-bar');
+    
+    const progressBarEffect = () => {
+        let scrollValue = window.pageYOffset;
+        let portfolioWrapperHeight = portfolioWrapper.clientHeight - window.innerHeight
+
+        let progressPercentage = (scrollValue / portfolioWrapperHeight) * 100;
+
+        progressBar.style.width = `${progressPercentage}%`;
+    };
+
+    window.addEventListener('scroll', progressBarEffect)
 });
 
